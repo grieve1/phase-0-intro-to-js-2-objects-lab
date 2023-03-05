2d1b0af3e20cffc5ab5b0effa27d3d3f7a478749
drake bishop <drakemx85@gmail.com>
2023-03-05 14:56:13 -0600

    created objects and changed their properties using destructive/nondestructive methods

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+    };
+}
+
+const employee = {
+    name: 'John Doe',
+    age: 25,
+    position: 'Developer'
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee
+}
